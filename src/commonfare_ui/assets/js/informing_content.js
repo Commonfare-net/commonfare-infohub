0c8821c80343965544be2d8e61b220ee32ba0f35
Luca Capra <luca.capra@gmail.com>
2017-02-17 14:01:47 +0100

    initial setup

--- src/commonfare_ui/assets/js/informing_content.js
+++ src/commonfare_ui/assets/js/informing_content.js
@@ -1,152 +1,67 @@
-//
-// var handleContent = function ($, block) {
-//
-//   if(!block.size()) return;
-//   if(block.is(".processed")) return;
-//   block.addClass("processed");
-//
-//   var termItem = block.find(".views-row")
-//
-//   if(termItem.size() <= 1) return;
-//
-//   var getNextIdx = function(dir, curr_idx) {
-//
-//     if (curr_idx === undefined) {
-//       curr_idx = termItem.index(termItem.filter(':visible').eq(0));
-//     }
-//
-//     var idx = curr_idx + dir
-//
-//     if(idx == termItem.size())
-//       idx = 0;
-//     if(idx == -1)
-//       idx = termItem.size() - 1;
-//
-//     return idx
-//   }
-//
-//   termItem.hide().eq(0).show()
-//
-//   termItem.find('.title a').on('click', false)
-//   termItem.on("click", function(ev) {
-//     document.location = $(this).find('.title a').attr('href')
-//     return false
-//   })
-//
-//   termItem.hammer({
-//     dragLockToAxis: true,
-//     dragBlockHorizontal: true
-//   }).bind("swipeleft swiperight", function (ev) {
-//
-//     var dir = ev.type == "swipeleft" ? 1 : -1;
-//
-//     var curr_idx = termItem.index($(ev.target));
-//     var idx = getNextIdx(dir, curr_idx)
-//
-//     var curr = termItem.eq(curr_idx)
-//     var next = termItem.eq(idx)
-//
-//     next.show()
-//     curr.css({
-//       position: "absolute"
-//     }).animate({
-//       left: dir === 1 ? -100 : 100,
-//       opacity: 0
-//     }, 200, function () {
-//
-//       curr.hide().css({
-//         position: "inherit",
-//         left: 0,
-//         right: 0,
-//         opacity: 1
-//       });
-//
-//     })
-//
-//
-//     return true;
-//   });
-//
-// }
-//
-// var setTitle = function($, title) {
-//
-//   var c = $('#mobile-title')
-//   if (c.size()) {
-//     c.empty()
-//     c.append(title)
-//     return
-//   }
-//
-//   c = $("<div id='mobile-title'></div>")
-//   c.append(title)
-//   $(".region-header").append(c)
-//
-// };
-//
-// var setContentHeight = function($) {
-//   var ww = $(".welfare-wrapper")
-//   ww.height(window.informing.getContentHeight() - 120)
-// }
-//
-// //entry point for informing
-// Drupal.behaviors.informing_content_list = {
-//   attach: function (context, settings) {
-//     jQuery(function ($) {
-//
-//       var block = $("#block-commonfare-content .welfare-info");
-//
-//       if(!block.size()) return
-//
-//       if(!isMobile()) {
-//
-//         var termItem = block.find(".views-row")
-//         termItem.find('.title a').on('click', false)
-//         termItem.on("click", function(ev) {
-//           document.location = $(this).find('.title a').attr('href')
-//           return false
-//         })
-//
-//         // align blocks height
-//         var maxh = 0
-//         var w = block.find('.welfare-wrapper')
-//         w.each(function() {
-//           var _h = $(this).height()
-//           maxh = _h > maxh ? _h : maxh
-//         })
-//
-//         w.height(maxh)
-//
-//         return
-//       }
-//
-//       if(block.is(".processed-c")) return
-//       block.addClass("processed-c")
-//
-//       handleContent($, block)
-//
-//       var title = $("#page-title h1").text()
-//       setTitle($, title)
-//
-//       setContentHeight($)
-//
-//     })
-//   }
-// };
+
+Drupal.behaviors.informing_mobile_accordion = {
+  attach: function (context, settings) {
+    jQuery(function ($) {
+
+      var block = $(".path-taxonomy .card-content .card-item");
+      if(!block.size()) return
+      if(block.is(".processed-mobile-menu")) return
+      block.addClass("processed-mobile-menu")
+
+      block.find('h2').on("click", function () {
+
+        if ($(this).parent().next().is(':visible')) {
+          document.location  = block.find('h2').parent().next().next().find('a').attr('href')
+        }
+
+        block.find('h2').parent().next().hide()
+        block.find('h2').parent().next().next().hide()
+
+        $(this).parent().next().show()
+        $(this).parent().next().next().show()
+
+        return false
+      })
+
+    })
+  }
+}
+
+Drupal.behaviors.informing_mobile_menu = {
+  attach: function (context, settings) {
+    jQuery(function ($) {
+
+      var block = $(".mobile-menu");
+      if(!block.size()) return
+      if(block.is(".processed-mobile-menu")) return
+      block.addClass("processed-mobile-menu")
+
+      block.on("click", function () {
+        $(".mobile-menu-item").show()
+      })
+
+      $(".mobile-menu-item .close-button").on("click", function () {
+        $(".mobile-menu-item ").fadeOut()
+      })
+
+
+    })
+  }
+}
 
 Drupal.behaviors.informing_home_scrolling = {
   attach: function (context, settings) {
     jQuery(function ($) {
 
       var block = $(".commonfare-homepage");
-
       if(!block.size()) return
       if(block.is(".processed-commonfare-homepage")) return
       block.addClass("processed-commonfare-homepage")
 
       var alist = $(".menu-base-theme li a");
+      var mm = $('.mobile-menu-item')
 
-      alist.eq(1).parent().addClass("active")
+      alist.eq(isMobile() ? 0 : 1).parent().addClass("active")
 
       block.onepage_scroll({
          sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
@@ -156,29 +71,40 @@ Drupal.behaviors.informing_home_scrolling = {
          pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
          updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
          beforeMove: function(index) {
-           if (index !== 1) {
+
+          if(mm.size() && mm.is(':visible')) {
+             mm.hide()
+          }
+
+           var idx = 1
+           if (index !== idx) {
              $('.about-main-header').fadeOut()
+             $('.main-header').fadeIn()
            }
          },  // This option accepts a callback function. The function will be called before the page moves.
          afterMove: function(index) {
-
-           if (index === 1) {
+           var idx = 1
+           if (index === idx) {
              $('.about-main-header').fadeIn()
+             $('.main-header').fadeOut()
            }
-
            alist.parent().removeClass("active")
-           alist.eq(index-1).parent().addClass("active")
-
+           var idx = 1;
+           alist.eq(index - idx).parent().addClass("active")
          },   // This option accepts a callback function. The function will be called after the page moves.
          loop: true,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
          keyboard: true,                  // You can activate the keyboard controls
-         responsiveFallback: 600,        // You can fallback to normal page scroll by defining the width of the browser in which
+         responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
                                           // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
                                           // the browser's width is less than 600, the fallback will kick in.
          direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".
       });
 
+      $('body').height($(window).height())
+      $('body').addClass('with-onepager');
+
       $('.about-main-header').show()
+      $('.main-header').hide()
 
       $("#about .button_how").on('click', function() {
         // document.location = document.location.toString().substr(0, document.location.toString().indexOf("#")) + "#get-informed"
@@ -206,7 +132,6 @@ Drupal.behaviors.informing_home_scrolling = {
         case "#participate":
           block.moveTo(3)
           break;
-        default:
       }
 
     })
@@ -246,9 +171,7 @@ Drupal.behaviors.informing_link_cards = {
   attach: function (context, settings) {
     jQuery(function ($) {
 
-      if(isMobile()) return;
-
-      var block = $(".commonfare-homepage .card-content .card-item .wrapper, .path-taxonomy .card-content .card-item .wrapper");
+      var block = $(".card-content .card-item .wrapper");
 
       if(!block.size()) return
       if(block.is(".processed-nn")) return
@@ -261,46 +184,34 @@ Drupal.behaviors.informing_link_cards = {
     })
   }
 };
-Drupal.behaviors.informing_fit_home = {
+Drupal.behaviors.informing_country_form = {
   attach: function (context, settings) {
     jQuery(function ($) {
 
-      // if(isMobile()) return;
-
-      var block = $(".commonfare-homepage, .path-taxonomy");
+      var block = $(".country-form form");
 
       if(!block.size()) return
-      if(block.is(".processed-fit")) return
-      block.addClass("processed-fit")
+      if(block.is(".processed-country-form")) return
+      block.addClass("processed-country-form")
 
-      // if(block.hasClass('path-taxonomy')) {
-      //
-      //   var h = $(window).height() - ($("#page-title").height() + $('header.main-header').height() + $('.copyright').height())
-      //   block.find('#block-commonfare-content').css({
-      //     'min-height': h
-      //   })
-      //   return;
-      // }
-      //
-      // var about = $("#about"),
-      //     get_informed = $("#get-informed"),
-      //     participate = $("#participate"),
-      //     admin = $('#toolbar-administration'),
-      //     screens = [ about, get_informed, participate  ],
-      //     win = $(window)
-      //
-      // var h = win.height()
-      // if (admin.size()) {
-      //   h -= admin.height()
-      // }
-      //
-      // screens.forEach(function(screen) {
-      //   screen.height(h)
-      // })
+      block.hide()
 
-      // about.on('click', function() {
-      //   document.location = document.location.toString().substr(0, document.location.toString().indexOf("#")) + "#get-informed"
-      // })
+      var countrySelector = $('.country-selector')
+
+      var lbl = countrySelector.find('.btn .label')
+      lbl.text( Drupal.t(lbl.text()) )
+
+      countrySelector.find('.dropdown-menu li a')
+      .on("click", function() {
+        var lang = $(this).attr("href").substr(1)
+        block.find('select').val(lang)
+        block.find('input[type="submit"]').trigger('click')
+      })
+      .each(function(i, el) {
+        $(this).text( Drupal.t($(this).text()) )
+      })
+
+      countrySelector.removeClass('hidden')
 
     })
   }
@@ -308,8 +219,6 @@ Drupal.behaviors.informing_fit_home = {
 Drupal.behaviors.informing_node_next_prev = {
   attach: function (context, settings) {
     jQuery(function ($) {
-
-      if(isMobile()) return;
 
       var block = $("#block-views-block-measure-list-by-term-block-1");
 
